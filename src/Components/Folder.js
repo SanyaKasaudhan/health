df2236263112bf1dfc131768d3770557633e72f4
SANYA KASAUDHAN <kasaudhansanya@gmail.com>
2022-12-05 09:51:42 +0530

    folder

--- src/Components/Folder.js
+++ src/Components/Folder.js
@@ -1,14 +1,28 @@
 import React, { useState } from "react";
-
+import './Folder.css'
 const Folder = ({ data }) => {
   const [expand, setExpand] = useState(false);
+  const[showInput, setShowInput]=useState({
+    visible:false,
+    isFolder:null 
+  });
+
+  const handleNewFolder = (e) =>{
+    e.stopPropagation();
+  }
   if (data.isFolder) {
     return (
+    <div>
       <div className="folder" onClick={() => setExpand(!expand)}>
-        📁{data.name}
+       <span>📁{data.name}</span> 
+       <div>
+        <button onClick={(e)=> handleNewFolder(e)}>Folder +</button>
+        <button onClick={(e)=> handleNewFolder(e)}>File +</button>
+       </div>
+       </div>
         <div style={{ display: expand ? "block" : "none", paddingLeft: 25 }}>
           {data.items.map((da) => {
-            return <Folder data={da} />;
+            return <Folder data={da} key={da.id} />;
           })}
         </div>
       </div>
